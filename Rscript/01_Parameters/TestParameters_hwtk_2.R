edf65c2bf83328d72b44a1c590fc829f31769317
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2022-06-20 18:29:11 +0200

    update: C sample from AB

--- Rscript/01_Parameters/TestParameters_hwtk_2.R
+++ Rscript/01_Parameters/TestParameters_hwtk_2.R
@@ -1,22 +1,22 @@
 ### starting parameters
 N        <- 200000
-prop_exp <- 0.2
-pi_ne    <- 0.05
-risk     <- 2
+prop_exp <- h
+pi_ne    <- w
+risk     <- t
 pi_e     <- pi_ne*risk
 
 ### validation indices: SE_AUB
 SE_AUB           <- 0.95
 
 ### validation indices: E=1 & Y=1
-SE_A_e           <- 0.5
+SE_A_e           <- k
 SE_B_given_A_e   <- 0.8
-
+ 
 SE_A_int_B_e       <- SE_A_e * SE_B_given_A_e
 SE_B_e             <- SE_AUB - SE_A_e + SE_A_int_B_e
 SE_A_given_B_e     <- SE_A_int_B_e / SE_B_e
 SE_B_given_not_A_e <- (SE_B_e - SE_A_int_B_e) / (1 - SE_A_e)
-
+  
 ### validation indices: E=0 & Y=1
 SE_A_ne          <- 0.5
 SE_B_given_A_ne  <- 0.8
@@ -34,10 +34,11 @@ SP_B_e           <- 0.98
 SP_A_ne          <- 0.99
 SP_B_ne          <- 0.98
 
+
 ### Bootstrap parameters
-na    = 250  # A sample size
-nb    = 250  # B sample size
-nc    = 250  # C sample size
+na    = z  # A sample size
+nb    = z  # B sample size
+nc    = z
 nsam  = 1000 # Monte Carlo rep
 nboot = 500  # Bootstrap sample size
 
